2397dc57ed90c9972aa17253fa56c730628b2a1d
leto-atreides-2 <64028599+leto-atreides-2@users.noreply.github.com>
2024-04-01 10:59:05 +0200

    Update main.py
    Validation set name

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ from utils import prepare_data, generate_val_csv, load_float_from_file
 from torchmetrics.classification import BinaryCalibrationError, BinaryAUROC, BinaryF1Score
 
 
-
 def main(args):
     """
     Main function to train, validate, and test the DiamondModel for predicting the apparition of central diabetic edema.
@@ -32,7 +31,6 @@ def main(args):
     # Determine if CUDA (GPU) is available, otherwise use CPU
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
     # Initialize TensorBoard writer
     # Important : You should keep Tensoroard to get the Yaml file of your running
     log_dir = f'./logs/{args.submission_name}/{args.backbone}'
@@ -43,7 +41,7 @@ def main(args):
 
     writer = SummaryWriter(log_dir)  # Specify your log directory
 
-    # Initialize metrics 
+    # Initialize metrics
     AUC = BinaryAUROC(thresholds=None).to(device)
     F1 = BinaryF1Score().to(device)
     ECE = BinaryCalibrationError(n_bins=10, norm='l1').to(device)
@@ -60,24 +58,22 @@ def main(args):
     # Load dataframes
     df_train = pd.read_csv(args.train_csv)
     df_val = pd.read_csv(args.val_csv)
-    
+
     train_dataset = DiamondDataset(df_train, mode = 'train', args=args)
     val_dataset = DiamondDataset(df_val, mode = 'val', args=args)
 
-    
     # dataloaders
     train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True,
                           num_workers=args.num_workers)
     val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False,
                           num_workers=args.num_workers)
 
-        
     # Model Initialization
     model = DiamondModel(args)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=float(args.learning_rate), weight_decay= float(args.weight_decay))
     model.to(device)
-    
+
     best_loss = 999
 
     # Checkpoint directory
@@ -87,7 +83,7 @@ def main(args):
     if not os.path.exists(ckpt_dir):
         # If it doesn't exist, create it
         os.makedirs(ckpt_dir)
-    
+
     for epoch in range(args.epochs):
 
         AUC.reset()
@@ -95,7 +91,7 @@ def main(args):
         ECE.reset()
 
         avg_loss_list = []
-        
+
         # Training step
         model.train()
         with torch.enable_grad():
@@ -109,27 +105,26 @@ def main(args):
                 AUC.update(probs[:, 1], labels)
                 F1.update(probs[:, 1], labels)
                 ECE.update(probs[:, 1], labels)
-                
+
                 loss.backward()
                 optimizer.step()
-                
+
                 avg_loss_list.append(loss.item())
-                
+
             avg_loss_train = np.array(avg_loss_list).mean()
-            
+
             # Log training loss and metrics
             writer.add_scalar('Loss/Train', avg_loss_train, epoch)
             writer.add_scalar('Metric/AUC_Train', AUC.compute(), epoch)
             writer.add_scalar('Metric/F1_Train', F1.compute(), epoch)
             writer.add_scalar('Metric/ECE_Train', ECE.compute(), epoch)
-            
+
             avg_loss_list = []
-            
+
             # Add other metrics
             
             print("[TRAIN] epoch={}/{}, Loss = {}".format(epoch, args.epochs, avg_loss_train))
-            
-        
+ 
         # Validation step
         model.eval()
 
@@ -148,19 +143,18 @@ def main(args):
                 AUC.update(probs[:, 1], labels)
                 F1.update(probs[:, 1], labels)
                 ECE.update(probs[:, 1], labels)
-            
+
             avg_loss_val = np.array(avg_loss_list).mean()
             # Log validation loss and metrics
             writer.add_scalar('Loss/Validation', avg_loss_val, epoch)
             writer.add_scalar('Metric/AUC_Val', AUC.compute(), epoch)
             writer.add_scalar('Metric/F1_Val', F1.compute(), epoch)
             writer.add_scalar('Metric/ECE_Val', ECE.compute(), epoch)
-            
+
             # Add other metrics
             
             print("[VAL] epoch={}/{}, Loss = {}".format(epoch, args.epochs, avg_loss_val))
-            
-        
+
         # Save best model
         if avg_loss_val < best_loss:
             print('Saving best model based on Loss, in epoch = ',epoch)
@@ -186,11 +180,10 @@ def main(args):
     writer.close()
 
 
-    
 if __name__ == "__main__":
     """
     Parses command-line arguments and runs the main training, validation, and testing pipeline for the DiamondModel.
-    
+
     Command-line arguments include learning rate, weight decay, batch size, number of epochs, paths to dataset files, etc.
     """
 
@@ -202,13 +195,10 @@ if __name__ == "__main__":
     parser.add_argument('--weight_decay', type=float, default=1e-4, help='weight decay')
     parser.add_argument('--batch_size', type=int, default=4, help='Batch size')
     parser.add_argument('--epochs', type=int, default=10, help='Number of epochs to train for')
-    parser.add_argument('--root_dir', type=str, default='./diamond_data', help='Path to the dataset') #./path/to/DeepDRiD
+    parser.add_argument('--root_dir', type=str, default='./diamond_data', help='Path to the dataset')
     parser.add_argument('--train_csv', type=str, default='training_set.csv', help='Path to the training CSV')
-    parser.add_argument('--val_csv', type=str, default='test_set.csv', help='Path to the validation CSV')
+    parser.add_argument('--val_csv', type=str, default='validation_set.csv', help='Path to the validation CSV')
     parser.add_argument('--num_workers', type=int, default=2, help='Number of workers')
-    
-    
-    
 
     args = parser.parse_args()
     main(args)
